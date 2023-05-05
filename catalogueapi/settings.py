cfa4c0dcb9ce3d74fd30df99be1a1250ac1d80d9
Cesar San Lucas <csanlucas@live.com>
2023-05-05 11:16:18 -0500

    REST api List all products Search products based in parameters Filtering product by SKU

--- catalogueapi/settings.py
+++ catalogueapi/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'product',
 ]
 
@@ -143,6 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins, viewsets
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from catalogueapi.utils.pagination import StandardResultsSetPagination
+from product.models import Product
+from product.serializers import ProductSerializer
+
+class ProductViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ("sku",)
+    search_fields = ("name", "sku", "id_source",)
